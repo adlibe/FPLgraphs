83804184dcb111dd3c7663a09b6f364adc178e51
adlibe <adrianbergersen@outlook.com>
2024-10-14 22:39:31 +0200

    added 'submit' functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { FormEvent, useState } from 'react';
 import './App.css'
 
-function App() {
+export default function App() {
+  const [submitted, setSubmitted] = useState(false);
 
+  function handleform(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+
+    setSubmitted(true);
+  }
   return (
     <>
-      <h1>FPLgraphs</h1>
+      <h1 className={submitted ? 'header-move' : ''}>FPLgraphs</h1>
+      {!submitted && (
+      <form method="post" onSubmit={handleform}>
+        <label>
+          Your FPL team ID: <input type="text" name="idform" />
+        </label>
+        <button type="submit">Submit ID</button>
+      </form>
+    )}
     </>
   )
 }
 
-export default App
